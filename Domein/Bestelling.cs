da89c911ab51a581a48ceef92e4c053812da4a43
milatxx <146560367+milatxx@users.noreply.github.com>
2025-10-01 16:00:37 +0200

    feat: generische klasse Bestelling<T> en IdGenerator toegevoegd

---
+++ Domein/Bestelling.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Boekwinkel.Services;
+
+namespace Boekwinkel.Domein
+{
+    public enum Aboperiode { EenMaand = 1, DrieMaanden = 3, TwaalfMaanden = 12 }
+
+    public class Bestelling<T> where T : Boek
+    {
+        private int _id;
+        public int Id
+        {
+            get => _id;
+            private set => _id = (value == 0) ? IdGenerator.Next() : value; // unieke toekenning
+        }
+
+        public T Item { get; }
+        public DateTime Datum { get; }
+        public int Aantal { get; set; }
+        public Aboperiode? Periode { get; set; } // voor tijdschriften
+
+        public Bestelling(T item, int aantal, Aboperiode? periode = null)
+        {
+            Id = 0;
+            Item = item;
+            Aantal = Math.Max(1, aantal);
+            Periode = periode;
+            Datum = DateTime.Now;
+        }
+
+        // Tuple: (ISBN, Aantal, TotalePrijs)
+        public (string Isbn, int Aantal, decimal TotalePrijs) Bestel()
+        {
+            decimal totaal = Item.Prijs * Aantal;
+
+            if (Item is Tijdschrift ts && Periode.HasValue)
+            {
+                int perMaand = ts.Frequentie switch
+                {
+                    Frequentie.Dagelijks => 30,
+                    Frequentie.Wekelijks => 4,
+                    _ => 1
+                };
+                int maanden = (int)Periode.Value;
+                int verschijningen = perMaand * maanden;
+                totaal = ts.Prijs * Aantal * verschijningen;
+            }
+
+            return (Item.Isbn, Aantal, decimal.Round(totaal, 2));
+        }
+    }
+}
---
+++ Services/IdGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+
+namespace Boekwinkel.Services
+{
+    public static class IdGenerator
+    {
+        private static int _laatste;
+        public static int Next() => Interlocked.Increment(ref _laatste);
+    }
+}
